aa8becec6098fad4f0d5adaf0a487b4118a0dc01
shgnplaatjies <plaatjiesshagan@gmail.com>
2022-07-15 06:09:03 +0200

    Checking for null data.

---
+++ Reach Analyzer.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns  
+import plotly.express as px
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import PassiveAggressiveRegressor
+
+data = pd.read_csv('Instagram.csv', encoding='latin-1')
+print(data.isnull().sum())
