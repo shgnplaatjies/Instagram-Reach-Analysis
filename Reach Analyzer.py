7367862546fbe9972dfc36115bf7b8008f4367d5
shgnplaatjies <plaatjiesshagan@gmail.com>
2022-07-16 14:47:27 +0200

    Updated Reach Analyzer
    Added comments and a condition to only remove null dat if there is null data.

--- Reach Analyzer.py
+++ Reach Analyzer.py
@@ -8,9 +8,15 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.linear_model import PassiveAggressiveRegressor
 
-data = pd.read_csv('Instagram.csv', encoding='latin-1')
+data = pd.read_csv('Instagram.csv', encoding='latin-1') # Read the data
+print("Checking for null values in the data")
+if data.isnull().sum().sum() > 0:
+    print("Found null values in the data\n",data.isnull().sum()) # Check for null values 
+    data.dropna(inplace=True) # Drop the null values
+    print("Dropping null values from the data\n",data.isnull().sum()) # Check for null values
+else:
+    print("No null values found in the data")
 
-data = data.dropna()
 
+print("Checking for data types",data.info()) # Check for data types
 
-print(data.info())
